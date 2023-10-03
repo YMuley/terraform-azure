6946a53699fffb2e3eeda479c8b508c33912a677
Yogesh.Muley <yogesh.muley@motifworks.com>
2023-10-03 18:39:58 +0530

    new commit

--- provider.tf
+++ provider.tf
@@ -13,5 +13,10 @@ provider "azurerm" {
       prevent_deletion_if_contains_resources = true
     }
     }
+
+  subscription_id = var.subscription_id
+  client_id = var.client_id
+  client_secret = var.client_secret
+  tenant_id = var.tenant_id
   #storage_use_azuread = true
 }
--- variable.tf
+++ variable.tf
@@ -9,3 +9,19 @@ variable "virtual_network_list" {
   default = []
 }
 
+variable "subscription_id" {
+  type = string
+  
+}
+
+variable "client_id" {
+  type = string
+}
+
+variable "client_secret" {
+  type = string
+}
+
+variable "tenant_id" {
+  type = string
+}
