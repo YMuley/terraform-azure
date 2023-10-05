18088d0aafeeadfdf97845e3dabec6c9409e1cc0
vijayispiro <124652645+vijayispiro@users.noreply.github.com>
2023-10-05 17:22:48 +0530

    Delete output.tf

--- output.tf
+++
@@ -1,9 +0,0 @@
-output "resource_group_output" {
-  value = module.resource_group.resource_group_output
-   
-}
-
-output "virtual_network_output" {
-  value = module.virtual_network.virtual_network_output
-    
-}
