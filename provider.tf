47224f99312ad68490ff8ff385e34bffc325b778
vijayispiro <124652645+vijayispiro@users.noreply.github.com>
2023-10-05 16:04:46 +0530

    Update provider.tf

--- provider.tf
+++ provider.tf
@@ -17,8 +17,12 @@ provider "azurerm" {
       prevent_deletion_if_contains_resources = true
     }
     }
+  client_id       = "ea7d721a-3358-433d-b26f-1d79421f920d"
+  client_secret   = "eMZ8Q~j_-WNd~gJ7i6GqBUxEpUsOcl9L8bpucbxP"
+  tenant_id       = "fd41ee0d-0d97-4102-9a50-c7c3c5470454"
+  subscription_id = "8694217e-4a30-4107-9a12-aeac74b82f5c"
   #storage_use_azuread = true
-  skip_provider_registration = true
+  #skip_provider_registration = true
   # ARM_SUBSCRIPTION_ID = var.ARM_SUBSCRIPTION_ID
   # ARM_CLIENT_ID = var.ARM_CLIENT_ID
   # ARM_CLIENT_SECRET = var.ARM_CLIENT_SECRET
